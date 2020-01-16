84b9a23fb1a6159be50c5b4d2729fd61ea81b8c9
MrMotivatorMan <58783463+MrMotivatorMan@users.noreply.github.com>
2020-01-16 14:51:23 +0000

    try to stop caching

--- app.py
+++ app.py
@@ -8,7 +8,18 @@ app = Flask(__name__)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+app.config["DEBUG"] = True
 Session(app)
+
+if app.config["DEBUG"]:
+    @app.after_request
+    def after_request(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, public, max-age=0"
+        response.headers["Expires"] = 0
+        response.headers["Pragma"] = "no-cache"
+        return response
+
+
 
 @app.route("/")
 def index():
